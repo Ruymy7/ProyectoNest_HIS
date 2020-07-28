f416285363d22e196844f0b1da111307aa5853e2
Ruymán <ruyman.padronc@gmail.com>
2020-07-28 11:29:40 +0100

    Change register path

--- src/register/controllers/register.controller.ts
+++ src/register/controllers/register.controller.ts
@@ -2,12 +2,12 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { RegisterService } from '../app/services/register.service';
 import { RegisterUserDto } from '../domain/dto/register-user-dto';
 
-@Controller('login')
+@Controller('register')
 export class RegisterController {
 
   constructor(private registerService: RegisterService) { }
 
-  @Post('new')
+  @Post('')
   registerUser(@Body() registerUserDto: RegisterUserDto): Promise<void> {
     return this.registerService.registerUser(registerUserDto)
   }
